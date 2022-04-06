dd66bcd8f8c6b0d796689dcf355b893d288f478e
Hajime Fujii <fujiihyrk@gmail.com>
2022-04-06 23:32:21 +0900

    FB link

--- js/common.js
+++ js/common.js
@@ -5,15 +5,15 @@ $(function () {
         timer: false,
         // ぼかし
         transition: 'blur',
-        transitionDuration: 10000,
-        delay: 10000,
+        transitionDuration: 8000,
+        delay: 8000,
         // サイズ大⇒小 動かす
         animation: 'kenburns',
-        animationDuration: 10000,
+        animationDuration: 8000,
     slides: [
-        { src: "./img_for_Pueo/Pueo_main_1-1.jpg" },
-        { src: "./img_for_Pueo/relax_3.jpg" },
+        { src: "./img_for_Pueo/Pueo_main_green_3-2.jpg" },
         { src: "./img_for_Pueo/beach_stone.JPG" },
+        { src: "./img_for_Pueo/relax_3_m.jpg" },
         ]
     });
 
